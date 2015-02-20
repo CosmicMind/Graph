24d1fde4910d6b981c9415adf1dc483cb369f6ff
Daniel Dahan <danieldahan@Macbook-Pro.local>
2015-02-19 21:20:22 -0500

    issue-24: udpated README

--- GraphKit/GraphKit/GKNodeProperty.swift
+++ GraphKit/GraphKit/GKNodeProperty.swift
@@ -25,7 +25,7 @@ import CoreData
 @objc(GKNodeProperty)
 internal class GKNodeProperty : NSManagedObject {
     @NSManaged internal var name: String
-    @NSManaged internal var value: AnyObject
+    @NSManaged internal var value: AnyObject?
 
     /**
     * init
