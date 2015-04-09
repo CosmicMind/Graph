2d930d563cd9f6f3664789090eca2b8a9ff5583b
Daniel Dahan <daniel@graphkit.io>
2015-04-09 18:13:46 -0400

    initial name revamp

--- Tests/GKEntitySearchTests.swift
+++ Tests/GKEntitySearchTests.swift
@@ -1,10 +1,20 @@
-//
-//  GKEntitySearchTests.swift
-//  GKGraphKit
-//
-//  Created by Daniel Dahan on 2015-04-03.
-//  Copyright (c) 2015 GraphKit, Inc. All rights reserved.
-//
+/**
+* Copyright (C) 2015 GraphKit, Inc. <http://graphkit.io> and other GraphKit contributors.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program located at the root of the software packnumeric
+* in a file called LICENSE.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 import XCTest
 import GKGraphKit
