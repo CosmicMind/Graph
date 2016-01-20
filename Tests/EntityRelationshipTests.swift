f1aa2958f73eeb5aba1a70155ddd20c92598dd8b
Daniel Dahan <daniel@graphkit.io>
2016-01-20 12:41:04 -0500

    adding Relationship tests for Entity

---
+++ Tests/EntityRelationshipTests.swift
@@ -0,0 +1,170 @@
+/*
+* Copyright (C) 2015 - 2016, Daniel Dahan and CosmicMind, Inc. <http://cosmicmind.io>.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+*	*	Redistributions of source code must retain the above copyright notice, this
+*		list of conditions and the following disclaimer.
+*
+*	*	Redistributions in binary form must reproduce the above copyright notice,
+*		this list of conditions and the following disclaimer in the documentation
+*		and/or other materials provided with the distribution.
+*
+*	*	Neither the name of GraphKit nor the names of its
+*		contributors may be used to endorse or promote products derived from
+*		this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+import XCTest
+@testable import GraphKit
+
+class EntityRelationshipTests : XCTestCase, GraphDelegate {
+	var graph: Graph!
+	
+	var saveExpectation: XCTestExpectation?
+	var insertExpectation: XCTestExpectation?
+	var insertPropertyExpectation: XCTestExpectation?
+	var insertGroupExpectation: XCTestExpectation?
+	var updatePropertyExpectation: XCTestExpectation?
+	var deleteExpectation: XCTestExpectation?
+	var deletePropertyExpectation: XCTestExpectation?
+	var deleteGroupExpectation: XCTestExpectation?
+	
+	override func setUp() {
+		super.setUp()
+		graph = Graph()
+		graph.delegate = self
+	}
+	
+	override func tearDown() {
+		graph = nil
+		super.tearDown()
+	}
+	
+	func testAll() {
+		graph.clear()
+//		graph.watchForEntity(types: ["T"], groups: ["G"], properties: ["P"])
+		
+		let e1: Entity = Entity(type: "T")
+		let e2: Entity = Entity(type: "T")
+		
+		let r1: Relationship = Relationship(type: "R")
+		r1.subject = e1
+		r1.object = e2
+		
+		XCTAssertEqual(r1, e1.relationshipsWhenSubject.first)
+		XCTAssertEqual(r1, e2.relationshipsWhenObject.first)
+		
+		graph.save { (success: Bool, error: NSError?) in
+			XCTAssertTrue(success, "Cannot save the Graph: \(error)")
+		}
+		
+		XCTAssertEqual(r1, e1.relationshipsWhenSubject.first)
+		XCTAssertEqual(r1, e2.relationshipsWhenObject.first)
+		
+//		XCTAssertEqual(NodeClass.Entity, n.nodeClass)
+//		
+//		saveExpectation = expectationWithDescription("Test: Save did not pass.")
+//		insertExpectation = expectationWithDescription("Test: Insert did not pass.")
+//		insertPropertyExpectation = expectationWithDescription("Test: Insert property did not pass.")
+//		insertGroupExpectation = expectationWithDescription("Test: Insert group did not pass.")
+//		
+//		graph.asyncSave { [unowned self] (success: Bool, error: NSError?) in
+//			XCTAssertTrue(success, "Cannot save the Graph: \(error)")
+//			self.saveExpectation?.fulfill()
+//		}
+//		
+//		waitForExpectationsWithTimeout(10, handler: nil)
+//		
+//		n["P"] = 222
+//		
+//		saveExpectation = expectationWithDescription("Test: Save did not pass.")
+//		updatePropertyExpectation = expectationWithDescription("Test: Update did not pass.")
+//		
+//		graph.asyncSave { [unowned self] (success: Bool, error: NSError?) in
+//			XCTAssertTrue(success, "Cannot save the Graph: \(error)")
+//			self.saveExpectation?.fulfill()
+//		}
+//		
+//		waitForExpectationsWithTimeout(10, handler: nil)
+//		
+//		n.delete()
+//		
+//		saveExpectation = expectationWithDescription("Test: Save did not pass.")
+//		deleteExpectation = expectationWithDescription("Test: Delete did not pass.")
+//		deletePropertyExpectation = expectationWithDescription("Test: Delete property did not pass.")
+//		deleteGroupExpectation = expectationWithDescription("Test: Delete group did not pass.")
+//		
+//		graph.asyncSave { [unowned self] (success: Bool, error: NSError?) in
+//			XCTAssertTrue(success, "Cannot save the Graph: \(error)")
+//			self.saveExpectation?.fulfill()
+//		}
+//		
+//		waitForExpectationsWithTimeout(10, handler: nil)
+	}
+	
+	func graphDidInsertEntity(graph: Graph, entity: Entity) {
+		XCTAssertEqual("T", entity.type)
+		XCTAssertEqual(111, entity["P"] as? Int)
+		XCTAssertTrue(entity.hasGroup("G"))
+		insertExpectation?.fulfill()
+	}
+	
+	func graphDidInsertEntityProperty(graph: Graph, entity: Entity, property: String, value: AnyObject) {
+		XCTAssertEqual("T", entity.type)
+		XCTAssertEqual("P", property)
+		XCTAssertEqual(111, value as? Int)
+		XCTAssertEqual(entity[property] as? Int, value as? Int)
+		insertPropertyExpectation?.fulfill()
+	}
+	
+	func graphDidInsertEntityGroup(graph: Graph, entity: Entity, group: String) {
+		XCTAssertEqual("T", entity.type)
+		XCTAssertEqual("G", group)
+		insertGroupExpectation?.fulfill()
+	}
+	
+	func graphDidUpdateEntityProperty(graph: Graph, entity: Entity, property: String, value: AnyObject) {
+		XCTAssertEqual("T", entity.type)
+		XCTAssertEqual("P", property)
+		XCTAssertEqual(222, value as? Int)
+		XCTAssertEqual(entity[property] as? Int, value as? Int)
+		updatePropertyExpectation?.fulfill()
+	}
+	
+	func graphDidDeleteEntity(graph: Graph, entity: Entity) {
+		XCTAssertEqual("T", entity.type)
+		deleteExpectation?.fulfill()
+	}
+	
+	func graphDidDeleteEntityProperty(graph: Graph, entity: Entity, property: String, value: AnyObject) {
+		XCTAssertEqual("T", entity.type)
+		XCTAssertEqual("P", property)
+		XCTAssertEqual(222, value as? Int)
+		deletePropertyExpectation?.fulfill()
+	}
+	
+	func graphDidDeleteEntityGroup(graph: Graph, entity: Entity, group: String) {
+		XCTAssertEqual("T", entity.type)
+		XCTAssertEqual("G", group)
+		deleteGroupExpectation?.fulfill()
+	}
+	
+	func testPerformance() {
+		self.measureBlock() {}
+	}
+}
+
