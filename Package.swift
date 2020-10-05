632ea50c82b5b17829505aa8a4e92f4db2bf54d0
Ryan Kohl <49837148+rkohl@users.noreply.github.com>
2020-10-05 17:32:31 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -23,8 +23,28 @@
  * THE SOFTWARE.
  */
 
-// swift-tools-version:3.1
+// swift-tools-version:5.1
 
 import PackageDescription
 
-let package = Package(name: "Graph")
+let package = Package(
+    name: "KDGDataGraph",
+    platforms: [
+        .iOS("14.0")
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "KDGDataGraph",
+            targets: ["KDGDataGraph"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "KDGDataGraph",
+            path: "Sources"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
