2c1b466720f91b9fda37f6035ee61709a2d84b3f
Ryan Kohl <kohldevelopmentgroup@icloud.com>
2020-10-15 21:46:40 -0500

    Updated to address "NSSecureUnarchiveFromData" depreciation. SPM updated, bumped min to iOS 12.

--- Package.swift
+++ Package.swift
@@ -1,48 +1,22 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (C) 2019, CosmicMind, Inc. <http://cosmicmind.com>.
- * All rights reserved.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-// swift-tools-version:5.1
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "KDGDataGraph",
+    name: "Graph",
     platforms: [
-        .iOS("14.0")
+        .iOS("12.0"),
+      .macOS("10.14")
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "KDGDataGraph",
-            targets: ["KDGDataGraph"]),
+            name: "Graph",
+            targets: ["Graph"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "KDGDataGraph",
+            name: "Graph",
             path: "Sources"
         ),
     ],
